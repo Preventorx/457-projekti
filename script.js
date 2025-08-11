f7fba5f07b8b40b5acfb11e5852fb48fecfb563e
Preventorx <preventorx@gmail.com>
2025-08-11 12:56:13 +0300

    Animaatio tykille ja ammukselle

--- script.js
+++ script.js
@@ -2,13 +2,37 @@ let pisteet = 0;
 
 const pisteetTulos = document.getElementById("pisteet");
 const tykki = document.getElementById("tykki");
+const ammuksetLaatikko = document.getElementById("ammukset");
+const vihuLista = document.getElementById("vihut");
+
+setInterval(luoVihu, 3000);
 
 tykki.addEventListener("click", () => {
-        pisteet++;
-        pisteetTulos.textContent = `Pisteet: ${pisteet}`;
-        tykki.classList.add("ammu");
+    ammuAmmus();
+    pisteet++;
+    pisteetTulos.textContent = `Pisteet: ${pisteet}`;
+    tykki.classList.add("ammu");
+});
 
-        setTimeout(() => {
-            tykki.classList.remove("ammu");
-        }, 500);
-    });
+
+function ammuAmmus() {
+    const ammus = document.createElement("div");
+    ammus.classList.add("ammus");
+    ammuksetLaatikko.appendChild(ammus);
+
+    setTimeout(() => {
+        tykki.classList.remove("ammu");
+        ammus.remove();
+    }, 500);
+}
+
+
+function luoVihu() {
+    const vihu = document.createElement("div");
+    vihu.classList.add("vihu");
+    vihuLista.appendChild(vihu);
+
+    setTimeout(() => {
+        vihu.remove();
+    }, 3000);
+}
