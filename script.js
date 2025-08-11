381780a69da6165b91767e0c298a2bd34c3fad22
Preventorx <preventorx@gmail.com>
2025-08-12 02:39:32 +0300

    Viholliset lisätty

--- script.js
+++ script.js
@@ -4,36 +4,66 @@ const pisteetTulos = document.getElementById("pisteet");
 const tykki = document.getElementById("tykki");
 const ammuksetLaatikko = document.getElementById("ammukset");
 const vihuLista = document.getElementById("vihut");
+const gameContainer = document.querySelector(".game-container");
 
-setInterval(luoVihu, 3000);
 
 tykki.addEventListener("click", () => {
     ammuAmmus();
     pisteet++;
     pisteetTulos.textContent = `Pisteet: ${pisteet}`;
     tykki.classList.add("ammu");
+
+    setTimeout(() => {
+        tykki.classList.remove("ammu");
+    }, 500);
 });
 
 
 function ammuAmmus() {
+    const tykkiRect = tykki.getBoundingClientRect();
     const ammus = document.createElement("div");
+
     ammus.classList.add("ammus");
     ammuksetLaatikko.appendChild(ammus);
 
     setTimeout(() => {
-        tykki.classList.remove("ammu");
         ammus.remove();
-    }, 500);
+    }, 1000);
 }
 
 
 function luoVihu() {
     const vihu = document.createElement("div");
+
     vihu.classList.add("vihu");
-    vihu.style.left = `${Math.random() * 240}px`;
+    vihu.style.left = `${Math.random() * (gameContainer.offsetWidth - 30)}px`;
     vihuLista.appendChild(vihu);
 
     setTimeout(() => {
         vihu.remove();
-    }, 3000);
+    }, 5000);
+}
+
+setInterval(luoVihu, 3000);
+
+setInterval(tarkistaOsuma, 100);
+
+
+function tarkistaOsuma() {
+    const ammukset = document.querySelectorAll(".ammus");
+    const vihut = document.querySelectorAll(".vihu");
+
+    ammukset.forEach(ammus => {
+        vihut.forEach(vihu => {
+            const ammusRect = ammus.getBoundingClientRect();
+            const vihuRect = vihu.getBoundingClientRect();
+
+            if (ammusRect.right > vihuRect.left && ammusRect.left < vihuRect.right && ammusRect.bottom > vihuRect.top && ammusRect.top < vihuRect.bottom) {
+                pisteet += 10;
+                pisteetTulos.textContent = `Pisteet: ${pisteet}`;
+                ammus.remove();
+                vihu.remove();
+            }
+        })
+    })
 }
